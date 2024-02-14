c069a8f98e398641c87f74126b6cc96489c84624
나원경 <97578485+hitriee@users.noreply.github.com>
2024-02-14 10:11:57 +0900

    Title: 1이 3개 이상 있는 위치, Time: 129ms, Memory: 25MB, Status: Passed - CodeTree

--- 240214/1이 3개 이상 있는 위치/place-more-than-3-ones.py
+++ 240214/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -1,12 +1,13 @@
 n = int(input())
 arr = [input().split() for _ in range(n)]
+dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
 cnt_position = 0
 
 for y in range(n):
     for x in range(n):
         cnt_near = 0
-        for dy, dx in (-1, 0), (0, -1), (1, 0), (0, 1):
-            ny, nx = y+dy, x+dx
+        for i in range(4):
+            ny, nx = y+dy[i], x+dx[i]
             if 0 <= ny < n and 0 <= nx < n and arr[ny][nx] == '1':
                 cnt_near += 1
         if cnt_near >= 3:
