e38d7dac8fc6ff2fcdac9a262f9f88fe3b95c501
나원경 <97578485+hitriee@users.noreply.github.com>
2024-02-14 10:29:37 +0900

    Title: 작은 구슬의 이동, Time: 126ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240214/작은 구슬의 이동/small-marble-movement.py
@@ -0,0 +1,21 @@
+n, t = map(int, input().split())
+r, c, d = input().split()
+r, c = map(int, [r, c])
+direction = {
+    'U': 0,
+    'R': 1,
+    'L': 2,
+    'D': 3,
+}
+dy, dx = [-1, 0, 0, 1], [0, 1, -1, 0]
+direct_idx = direction[d]
+now = 0
+
+for now in range(1, t+1):
+    nr, nc = r + dy[direct_idx], c + dx[direct_idx]
+    if not (1 <= nr <= n and 1 <= nc <= n):
+        direct_idx = 3 - direct_idx
+    else:
+        r, c = nr, nc
+
+print(r, c)
