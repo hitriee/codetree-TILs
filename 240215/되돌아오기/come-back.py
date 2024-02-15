266c236ef104c0744f3604009967feace287fea2
나원경 <97578485+hitriee@users.noreply.github.com>
2024-02-15 16:43:02 +0900

    Title: 되돌아오기, Time: 126ms, Memory: 24MB, Status: Passed - CodeTree

--- 240215/되돌아오기/come-back.py
+++ 240215/되돌아오기/come-back.py
@@ -1,27 +1,25 @@
-N = int(input())
-direction = {
-    'W': (-1, 0),
-    'S': (0, -1),
-    'N': (0, 1),
-    'E': (1, 0),
-}
-y = x = time = 0
+def find_time():
 
-for _ in range(N):
-    key, duration = input().split()
-    duration = int(duration)
-    dx, dy = direction[key]
-    for _ in range(duration):
-        x += dx
-        y += dy
-        time += 1
-        if x == y == 0:
-            print(time)
-            break
-    else:
-        continue
-    
-    break
+    N = int(input())
+    direction = {
+        'W': (-1, 0),
+        'S': (0, -1),
+        'N': (0, 1),
+        'E': (1, 0),
+    }
 
-else:
-    print(-1)
+    y = x = time = 0
+
+    for _ in range(N):
+        key, duration = input().split()
+        duration = int(duration)
+        dx, dy = direction[key]
+        for _ in range(duration):
+            x += dx
+            y += dy
+            time += 1
+            if x == y == 0:
+                return time
+    return -1
+
+print(find_time())
