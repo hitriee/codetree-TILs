5714a73d35847b0cda82d3d9a53f7d6ba7d7fc24
나원경 <97578485+hitriee@users.noreply.github.com>
2024-02-15 16:41:00 +0900

    Title: 되돌아오기, Time: 149ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240215/되돌아오기/come-back.py
@@ -0,0 +1,27 @@
+N = int(input())
+direction = {
+    'W': (-1, 0),
+    'S': (0, -1),
+    'N': (0, 1),
+    'E': (1, 0),
+}
+y = x = time = 0
+
+for _ in range(N):
+    key, duration = input().split()
+    duration = int(duration)
+    dx, dy = direction[key]
+    for _ in range(duration):
+        x += dx
+        y += dy
+        time += 1
+        if x == y == 0:
+            print(time)
+            break
+    else:
+        continue
+    
+    break
+
+else:
+    print(-1)
