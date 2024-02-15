176e96616e5c0c12c3073b1a4dc79c467c5eef22
나원경 <97578485+hitriee@users.noreply.github.com>
2024-02-15 13:48:18 +0900

    Title: 문자열 정렬, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240215/문자열 정렬/string-sort.py
+++ 240215/문자열 정렬/string-sort.py
@@ -1,13 +1,16 @@
 alp_string = input()
 
-alp_cnt = {chr(ord('a')+i):0 for i in range(26)}
+alp_cnt = {}
 
 for alp in alp_string:
-    alp_cnt[alp] += 1
+    if alp_cnt.get(alp):
+        alp_cnt[alp] += 1
+    else:
+        alp_cnt[alp] = 1
 
 new_string = ''
 
-for alp in alp_cnt:
+for alp in sorted(alp_cnt):
     new_string += alp*alp_cnt[alp]
 
 print(new_string, sep='')
