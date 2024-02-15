ecc54cb4d1c3eb602105018759390b78dd95b059
나원경 <97578485+hitriee@users.noreply.github.com>
2024-02-15 13:47:26 +0900

    Title: 문자열 정렬, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240215/문자열 정렬/string-sort.py
+++ 240215/문자열 정렬/string-sort.py
@@ -1,2 +1,13 @@
 alp_string = input()
-print(*sorted(alp_string), sep='')
+
+alp_cnt = {chr(ord('a')+i):0 for i in range(26)}
+
+for alp in alp_string:
+    alp_cnt[alp] += 1
+
+new_string = ''
+
+for alp in alp_cnt:
+    new_string += alp*alp_cnt[alp]
+
+print(new_string, sep='')
