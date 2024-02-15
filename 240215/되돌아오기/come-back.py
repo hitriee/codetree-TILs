da567cf9c863c9091b9f01afea1c7bce004af9ad
나원경 <97578485+hitriee@users.noreply.github.com>
2024-02-15 16:59:56 +0900

    Title: 되돌아오기, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 240215/되돌아오기/come-back.py
+++ 240215/되돌아오기/come-back.py
@@ -1,37 +1,36 @@
-def find_time():
+N = int(input())
+direction = {
+    'W': (-1, 0),
+    'S': (0, -1),
+    'N': (0, 1),
+    'E': (1, 0),
+}
 
-    N = int(input())
-    direction = {
-        'W': (-1, 0),
-        'S': (0, -1),
-        'N': (0, 1),
-        'E': (1, 0),
-    }
+key, duration = input().split()
+duration = int(duration)
+dx, dy = direction[key]
+y, x = dy * duration, dx * duration
+time = duration
 
+for _ in range(N-1):
     key, duration = input().split()
     duration = int(duration)
     dx, dy = direction[key]
-    y, x = dy * duration, dx * duration
-    time = duration
+    dx *= duration
+    dy *= duration
 
-    for _ in range(N-1):
-        key, duration = input().split()
-        duration = int(duration)
-        dx, dy = direction[key]
-        dx *= duration
-        dy *= duration
-
-        if x == 0:
-            if dy * y < 0 and abs(dy) >= abs(y):
-                return time+abs(y)
-        elif y == 0:
-            if dx * x < 0 and abs(dx) >= abs(x):
-                return time+abs(x)
-        
-        time += duration
-        x += dx
-        y += dy
+    if x == 0:
+        if dy * y < 0 and abs(dy) >= abs(y):
+            print(time+abs(y))
+            break
+    elif y == 0:
+        if dx * x < 0 and abs(dx) >= abs(x):
+            print(time+abs(x))
+            break
     
-    return -1
+    time += duration
+    x += dx
+    y += dy
 
-print(find_time())
+else:
+    print(-1)
