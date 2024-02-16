03cbffaa1aac24d132e6ba4387d6072d07d0d7e9
나원경 <97578485+hitriee@users.noreply.github.com>
2024-02-16 10:35:53 +0900

    Title: 줄 세우기 2, Time: 193ms, Memory: 25MB, Status: Passed - CodeTree

--- 240216/줄 세우기 2/line-up-students-2.py
+++ 240216/줄 세우기 2/line-up-students-2.py
@@ -1,11 +1,11 @@
 class Student:
     def __init__(self, height, weight, idx):
-        self.h = int(height)
-        self.w = int(weight)
+        self.h = height
+        self.w = weight
         self.i = idx
 
 N = int(input())
-students = [Student(*input().split(), i) for i in range(1, N+1)]
+students = [Student(*map(int, input().split()), i) for i in range(1, N+1)]
 students.sort(key=lambda student: (student.h, -student.w))
 
 for student in students:
