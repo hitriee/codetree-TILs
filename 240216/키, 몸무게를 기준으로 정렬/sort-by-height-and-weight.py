013e13b1944a469e94c63e8eee92491852f40d6d
나원경 <97578485+hitriee@users.noreply.github.com>
2024-02-16 10:15:34 +0900

    Title: 키, 몸무게를 기준으로 정렬, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

--- 240216/키, 몸무게를 기준으로 정렬/sort-by-height-and-weight.py
+++ 240216/키, 몸무게를 기준으로 정렬/sort-by-height-and-weight.py
@@ -1,5 +1,5 @@
 n = int(input())
-people = [input().split() for _ in range(n)]
+people = [tuple(input().split()) for _ in range(n)]
 people.sort(key=lambda person: (int(person[1]), -int(person[2])))
 
 for person in people:
