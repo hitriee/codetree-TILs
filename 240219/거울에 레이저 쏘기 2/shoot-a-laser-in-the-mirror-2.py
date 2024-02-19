1751a96d9c1ec363409196638c9a22eceee41762
나원경 <97578485+hitriee@users.noreply.github.com>
2024-02-19 15:13:36 +0900

    Title: 거울에 레이저 쏘기 2, Time: 137ms, Memory: 25MB, Status: Passed - CodeTree

--- 240219/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
+++ 240219/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -2,23 +2,20 @@ N = int(input())
 info = [input() for _ in range(N)]
 K = int(input()) - 1
 direction = [(1, 0), (0, -1), (-1, 0), (0, 1)]
-idx = K//N
-y = x = cnt = 0
-cnt, temp_i = 1, 3
+idx, cnt = K//N, 0
 
-for _ in range(K):
-    ny = y + direction[temp_i][0]
-    nx = x + direction[temp_i][1]
-    if 0 <= ny < N and 0 <= ny < N:
-        y, x = ny, nx
-    else:
-        temp_i = (temp_i + 1) % 4
-        y += direction[temp_i][0]
-        x += direction[temp_i][1]
+if idx == 0:
+    y, x = 0, K
+elif idx == 1:
+    y, x = K%N, N-1
+elif idx == 2:
+    y, x = N-1, N-1-(K%N)
+else:
+    y, x = N-1-(K%N), 0
 
-while 0 <= y < N and 0 <= y < N:
+while 0 <= y < N and 0 <= x < N:
     cnt += 1
-    if info[y][x] == '/':
+    if info[y][x] == '\\':
         idx = 3 - idx
     elif idx % 2:
         idx -= 1
