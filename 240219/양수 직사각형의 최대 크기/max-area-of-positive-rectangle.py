c6d5a22e3cfd8fbf54bb4b33ca6c492ad9a5cce1
나원경 <97578485+hitriee@users.noreply.github.com>
2024-02-19 13:26:14 +0900

    Title: 양수 직사각형의 최대 크기, Time: 126ms, Memory: 25MB, Status: Passed - CodeTree

--- 240219/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 240219/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -1,16 +1,16 @@
 n, m = map(int, input().split())
 rectangle = [tuple(map(int, input().split())) for _ in range(n)]
 max_size = -1
-check = [[0] * m for _ in range(n)]
+check = [[-1] * m for _ in range(n)]
 
 for i in range(n):
     cnt = 0
     for j in range(m-1, -1, -1):
         if rectangle[i][j] > 0:
             cnt += 1
+            check[i][j] = cnt
         else:
             cnt = 0
-        check[i][j] = cnt
 
 for i in range(n):
     for j in range(m):
