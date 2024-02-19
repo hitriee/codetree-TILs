9d503676a38a8c14f1252eec571d54be4273d155
나원경 <97578485+hitriee@users.noreply.github.com>
2024-02-19 10:39:28 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240219/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.py
@@ -0,0 +1,16 @@
+n, m = map(int, input().split())
+rectangle = [[0] * m for _ in range(n)]
+dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
+y = x = idx = 0
+
+for num in range(1, n*n+1):
+    rectangle[y][x] = num
+    ny, nx = y+dy[idx], x+dx[idx]
+    if 0 <= ny < n and 0 <= nx < m and rectangle[ny][nx] == 0:
+        y, x = ny, nx
+    else:
+        idx = (idx+1) % 4
+        y, x = y+dy[idx], x+dx[idx]
+
+for i in range(n):
+    print(*rectangle[i])
