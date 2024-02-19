7561bfc69d1db3bee8a5e810a0c7a1f04d97d415
나원경 <97578485+hitriee@users.noreply.github.com>
2024-02-19 16:00:28 +0900

    Title: 금 채굴하기, Time: 240ms, Memory: 26MB, Status: Passed - CodeTree

--- 240219/금 채굴하기/gold-mining.py
+++ 240219/금 채굴하기/gold-mining.py
@@ -1,33 +1,32 @@
 n, m = map(int, input().split())
 area = [input().split() for _ in range(n)]
+cost = [i*i + (i+1)*(i+1) for i in range(n+1)]
 max_cnt = 0
-max_num = max(n, m)
 dy, dx = [-1, 0, 1, 0], [0, -1, 0, 1]
 
 for y in range(n):
-    for x in range(m):
+    for x in range(n):
         each_cnt, gold_cnt = 1, 0
+        visited = [[False] * n for _ in range(n)]
+        visited[y][x] = True
         if area[y][x] == '1':
             gold_cnt += 1
             if max_cnt == 0:
                 max_cnt = 1
-        
-        for k in range(1, max_num):
-            new_gold_cnt = gold_cnt
-            for ny in range(max(0, y-k+1), min(y+k, n)):
-                for nx in range(max(0, x-k+1), min(x+k, m)):
-                    each_cnt += 1
-                    if area[ny][nx] == '1':
-                        gold_cnt += 1
-
-            for i in range(4):
-                ny, nx = y + dy[i] * k, x + dx[i] * k
-                if 0 <= ny < n and 0 <= nx < m:
-                    each_cnt += 1
-                    if area[ny][nx] == '1':
-                        new_gold_cnt += 1
-            
-            if new_gold_cnt * m >= each_cnt and max_cnt < new_gold_cnt:
-                max_cnt = new_gold_cnt
+        edge, temp = [(y, x)], []
+        for k in range(1, n+1):
+            for ny, nx in edge:
+                for i in range(4):
+                    nny, nnx = ny+dy[i], nx+dx[i]
+                    if 0 <= nny < n and 0 <= nnx < n and not visited[nny][nnx]:
+                        visited[nny][nnx] = True
+                        temp.append((nny, nnx))
+                        if area[nny][nnx] == '1':
+                            gold_cnt += 1
+            if max_cnt < gold_cnt and gold_cnt * m >= cost[k]:
+                max_cnt = gold_cnt
+            if not temp:
+                break
+            edge, temp = temp[:], []
 
 print(max_cnt)
