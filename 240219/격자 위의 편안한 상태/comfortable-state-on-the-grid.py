959037f81d424dbffb7dea5932742b107428b3ae
나원경 <97578485+hitriee@users.noreply.github.com>
2024-02-19 10:06:01 +0900

    Title: 격자 위의 편안한 상태, Time: 196ms, Memory: 26MB, Status: Passed - CodeTree

--- 240219/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
+++ 240219/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
@@ -1,12 +1,12 @@
 N, M = map(int, input().split())
-arr = [[0] * N for _ in range(N)]
-dr, dc = [-1, 0, -1, -2], [0, -1, -2, -1]
+arr = [[0] * (N+2) for _ in range(N+2)]
+dr, dc = [0, 1, 0, -1], [1, 0, -1, 0]
 for _ in range(M):
     r, c = map(int, input().split())
-    arr[r-1][c-1] = 1
+    arr[r][c] = 1
     cnt = 0
     for i in range(4):
         nr, nc = r+dr[i], c+dc[i]
-        if 0 <= nr < N and 0 <= nc < N and arr[nr][nc] == 1:
+        if arr[nr][nc] == 1:
             cnt += 1
     print(1 if cnt == 3 else 0)
