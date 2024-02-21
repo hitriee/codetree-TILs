0c37f70e154e73c59391a8f5cc56b3f9fa853a63
나원경 <97578485+hitriee@users.noreply.github.com>
2024-02-21 10:02:15 +0900

    Title: 컨베이어 벨트, Time: 120ms, Memory: 25MB, Status: Passed - CodeTree

--- 240221/컨베이어 벨트/conveyor-belt.py
+++ 240221/컨베이어 벨트/conveyor-belt.py
@@ -3,9 +3,12 @@ top_numbers = input().split()
 bottom_numbers = input().split()
 
 for _ in range(t):
-    last = top_numbers[-1:]
-    top_numbers = bottom_numbers[-1:] + top_numbers[:-1]
-    bottom_numbers = last + bottom_numbers[:-1]
+    last_top, last_bottom = top_numbers[-1], bottom_numbers[-1]
+    for i in range(n-1, 0, -1):
+        top_numbers[i] = top_numbers[i-1]
+        bottom_numbers[i] = bottom_numbers[i-1]
+    top_numbers[0] = last_bottom
+    bottom_numbers[0] = last_top
 
 print(' '.join(top_numbers))
 print(' '.join(bottom_numbers))
