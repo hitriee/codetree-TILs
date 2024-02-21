a16dedd9e3547651c0b46956c2e0f98eb75e8581
나원경 <97578485+hitriee@users.noreply.github.com>
2024-02-21 11:36:05 +0900

    Title: 1차원 바람, Time: 178ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240221/1차원 바람/The-1D-wind-blows.py
@@ -0,0 +1,45 @@
+N, M, Q = map(int, input().split())
+building = [input().split() for _ in range(N)]
+reversed_d = {'R':'L', 'L':'R'}
+
+def move_each(r, d):
+    if d == 'R':
+        start, end, step = 0, M-1, 1
+    else:
+        start, end, step = M-1, 0, -1
+    
+    temp = building[r][start]
+
+    for j in range(start, end, step):
+        building[r][j] = building[r][j+step]
+    
+    building[r][end] = temp
+
+def move(r, d):
+    r = int(r) - 1
+    move_each(r, d)
+    new_d = d
+    for i in range(r-1, -1, -1):
+        new_d = reversed_d[new_d]
+        for j in range(M):
+            if building[i+1][j] == building[i][j]:
+                move_each(i, new_d)
+                break
+        else:
+            break
+    
+    new_d = d
+    for i in range(r+1, N):
+        new_d = reversed_d[new_d]
+        for j in range(M):
+            if building[i-1][j] == building[i][j]:
+                move_each(i, new_d)
+                break
+        else:
+            break
+
+for _ in range(Q):
+    move(*input().split())
+
+for i in range(N):
+    print(' '.join(building[i]))
