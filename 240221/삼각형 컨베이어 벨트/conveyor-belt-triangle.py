485aa78c932ca3cede51dffde02c8e8973643b03
나원경 <97578485+hitriee@users.noreply.github.com>
2024-02-21 11:08:47 +0900

    Title: 삼각형 컨베이어 벨트, Time: 185ms, Memory: 25MB, Status: Passed - CodeTree

--- 240221/삼각형 컨베이어 벨트/conveyor-belt-triangle.py
+++ 240221/삼각형 컨베이어 벨트/conveyor-belt-triangle.py
@@ -1,17 +1,14 @@
 n, t = map(int, input().split())
-left = input().split()
-right = input().split()
-bottom = input().split()
+belt = [input().split() for _ in range(3)]
 
 for _ in range(t):
-    last_l, last_r, last_b = left[-1], right[-1], bottom[-1]
-    for i in range(n-1, 0, -1):
-        left[i] = left[i-1]
-        right[i] = right[i-1]
-        bottom[i] = bottom[i-1]
+    last_numbers = [belt[i][-1] for i in range(3)]
+    for j in range(n-1, 0, -1):
+        for i in range(3):
+            belt[i][j] = belt[i][j-1]
 
-    left[0], right[0], bottom[0] = last_b, last_l, last_r
+    for i in range(3):
+        belt[i][0] = last_numbers[(i-1)%3]
 
-print(' '.join(left))
-print(' '.join(right))
-print(' '.join(bottom))
+for i in range(3):
+    print(' '.join(belt[i]))
