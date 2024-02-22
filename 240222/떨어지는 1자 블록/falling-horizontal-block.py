c47c74bb175094e19ed17bb74a9f4bf0035bdd79
나원경 <97578485+hitriee@users.noreply.github.com>
2024-02-22 22:07:52 +0900

    Title: 떨어지는 1자 블록, Time: 140ms, Memory: 24MB, Status: Passed - CodeTree

--- 240222/떨어지는 1자 블록/falling-horizontal-block.py
+++ 240222/떨어지는 1자 블록/falling-horizontal-block.py
@@ -3,10 +3,10 @@ arr = [input().split() for _ in range(n)]
 block_i = n-1
 
 for j in range(k-1, m+k-1):
-    for i in range(1, n):
-        if arr[i][j] == '1':
-            if block_i > i-1:
-                block_i = i-1
+    for i in range(n-1):
+        if arr[i+1][j] == '1':
+            if block_i > i:
+                block_i = i
             break
     else:
         break
