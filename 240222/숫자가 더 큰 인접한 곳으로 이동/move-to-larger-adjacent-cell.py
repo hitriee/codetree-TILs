2165e44b80de803a6887e497a2873dc50c49fac2
나원경 <97578485+hitriee@users.noreply.github.com>
2024-02-22 21:42:19 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 156ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240222/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
@@ -0,0 +1,22 @@
+n, r, c = map(lambda x: int(x)-1, input().split())
+n += 1
+arr = [list(map(int, input().split())) for _ in range(n)]
+dr, dc = [-1, 1, 0, 0], [0, 0, -1, 1]
+numbers = []
+
+def in_range(y, x):
+    return 0 <= y < n and 0 <= x < n
+
+while True:
+    now = arr[r][c]
+    for i in range(4):
+        nr, nc = r+dr[i], c+dc[i]
+        if in_range(nr, nc) and now < arr[nr][nc]:
+            r, c = nr, nc
+            numbers.append(now)
+            break
+    else:
+        numbers.append(now)
+        break
+
+print(*numbers)
