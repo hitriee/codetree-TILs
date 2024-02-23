2fa57952f78fd2d62987d10c4bc7eb31777447c9
나원경 <97578485+hitriee@users.noreply.github.com>
2024-02-23 10:03:39 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240223/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
@@ -0,0 +1,33 @@
+N = int(input())
+y, x = map(lambda x: int(x)-1, input().split())
+dy, dx = [0, -1, 0, 1], [1, 0, -1, 0]
+arr = [input() for _ in range(N)]
+time, i = 1, 0
+initial_y, initial_x = y, x
+
+def in_range(y, x):
+    return 0 <= y < N and 0 <= x < N
+
+while True:
+    ny, nx = y+dy[i], x+dx[i]
+    if y == initial_y and x == initial_x and i == 0:
+        time = - 1
+        break
+    if not in_range(ny, nx):
+        break
+    if arr[ny][nx] == '#':
+        i = (i+1)%4
+    else:
+        time += 1
+        y, x = ny, nx
+        ni = (i-1)%4
+        ny += dy[ni]
+        nx += dx[ni]
+        if arr[ny][nx] != '#':
+            y, x, i = ny, nx, ni
+            time += 1
+        if initial_y == y and initial_x == x and i == 0:
+            time = -1
+            break
+
+print(time)
