7416ed7901a6ebe1341a2d0e84e0690ae1b7cbde
나원경 <97578485+hitriee@users.noreply.github.com>
2024-02-29 16:53:49 +0900

    Title: 단순한 동전 챙기기, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240229/단순한 동전 챙기기/collect-coins-easy.py
+++ 240229/단순한 동전 챙기기/collect-coins-easy.py
@@ -21,16 +21,18 @@ for i in range(8):
     coin1 = coin_list[i]
     if coin1:
         cnt1 = calc_distance(*S, *coin1)
-        for j in range(i+1, 9):
-            coin2 = coin_list[j]
-            if coin2:
-                cnt2 = cnt1 + calc_distance(*coin1, *coin2)
-                for k in range(j+1, 10):
-                    coin3 = coin_list[k]
-                    if coin3:
-                        cnt3 = cnt2 + calc_distance(*coin2, *coin3) + calc_distance(*coin3, *E)
-                        if min_cnt > cnt3:
-                            min_cnt = cnt3
+        if cnt1 < min_cnt:
+            for j in range(i+1, 9):
+                coin2 = coin_list[j]
+                if coin2:
+                    cnt2 = cnt1 + calc_distance(*coin1, *coin2)
+                    if cnt2 < min_cnt:
+                        for k in range(j+1, 10):
+                            coin3 = coin_list[k]
+                            if coin3:
+                                cnt3 = cnt2 + calc_distance(*coin2, *coin3) + calc_distance(*coin3, *E)
+                                if min_cnt > cnt3:
+                                    min_cnt = cnt3
 
 if min_cnt < 22*N:
     print(min_cnt)
