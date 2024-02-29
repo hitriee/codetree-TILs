ad6915494efdc51e90956cdbafe300739522ae1e
나원경 <97578485+hitriee@users.noreply.github.com>
2024-02-29 16:52:20 +0900

    Title: 단순한 동전 챙기기, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240229/단순한 동전 챙기기/collect-coins-easy.py
+++ 240229/단순한 동전 챙기기/collect-coins-easy.py
@@ -17,23 +17,6 @@ for i in range(N):
 def calc_distance(y1, x1, y2, x2):
     return abs(y1 - y2) + abs(x1 - x2)
 
-def choose_coin(level, cnt, before, start):
-    global min_cnt
-
-    if level == 3:
-        cnt += calc_distance(*before, *E)
-        if min_cnt > cnt:
-            min_cnt = cnt
-        return
-    
-    for i in range(start, 10):
-        coin = coin_list[i]
-        if coin:
-            choose_coin(level+1, cnt+calc_distance(*before, *coin), coin, i+1)
-
-
-choose_coin(0, 0, S, 0)
-
 for i in range(8):
     coin1 = coin_list[i]
     if coin1:
