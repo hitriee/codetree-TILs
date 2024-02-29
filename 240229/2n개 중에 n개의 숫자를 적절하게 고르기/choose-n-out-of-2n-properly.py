7507a6b2dc1498ecdc036ed525ea696b0d4ae510
나원경 <97578485+hitriee@users.noreply.github.com>
2024-02-29 17:05:28 +0900

    Title: 2n개 중에 n개의 숫자를 적절하게 고르기, Time: 182ms, Memory: 28MB, Status: Passed - CodeTree

---
+++ 240229/2n개 중에 n개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.py
@@ -0,0 +1,19 @@
+n = int(input())
+numbers = tuple(map(int, input().split()))
+min_dif = n * 1000
+total = sum(numbers)
+
+def split_numbers(level, result, start):
+    global min_dif
+
+    if level == n:
+        dif = abs(total - 2*result)
+        if min_dif > dif:
+            min_dif = dif
+        return
+    
+    for i in range(start, 2*n):
+        split_numbers(level+1, result+numbers[i], i+1)
+
+split_numbers(0, 0, 0)
+print(min_dif)
