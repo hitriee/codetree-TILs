6913539271d0768208984738826613620657b5c2
나원경 <97578485+hitriee@users.noreply.github.com>
2024-02-29 16:13:50 +0900

    Title: n개 중에 m개 뽑기, Time: 189ms, Memory: 25MB, Status: Passed - CodeTree

--- 240229/n개 중에 m개 뽑기/n-choose-m.py
+++ 240229/n개 중에 m개 뽑기/n-choose-m.py
@@ -1,15 +1,12 @@
 N, M = map(int, input().split())
-str_nums = tuple(map(str, range(1, N+1)))
 
 def find_comb(level, result, start):
     if level == M:
         print(result)
         return
     
-    for i in range(start, N):
-        num = str_nums[i]
-        find_comb(level+1, result+f' {num}', i+1)
+    for i in range(start, N+1):
+        find_comb(level+1, result+f' {str(i)}', i+1)
 
-for i in range(N):
-    num = str_nums[i]
-    find_comb(1, num, i+1)
+for i in range(1, N+1):
+    find_comb(1, str(i), i+1)
