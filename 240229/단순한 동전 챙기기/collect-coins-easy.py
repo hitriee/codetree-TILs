fa843d8a66c053aaa5b8b68e38d3b0f5de010f32
나원경 <97578485+hitriee@users.noreply.github.com>
2024-02-29 16:54:41 +0900

    Title: 단순한 동전 챙기기, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240229/단순한 동전 챙기기/collect-coins-easy.py
+++ 240229/단순한 동전 챙기기/collect-coins-easy.py
@@ -17,22 +17,24 @@ for i in range(N):
 def calc_distance(y1, x1, y2, x2):
     return abs(y1 - y2) + abs(x1 - x2)
 
-for i in range(8):
-    coin1 = coin_list[i]
-    if coin1:
-        cnt1 = calc_distance(*S, *coin1)
-        if cnt1 < min_cnt:
-            for j in range(i+1, 9):
-                coin2 = coin_list[j]
-                if coin2:
-                    cnt2 = cnt1 + calc_distance(*coin1, *coin2)
-                    if cnt2 < min_cnt:
-                        for k in range(j+1, 10):
-                            coin3 = coin_list[k]
-                            if coin3:
-                                cnt3 = cnt2 + calc_distance(*coin2, *coin3) + calc_distance(*coin3, *E)
-                                if min_cnt > cnt3:
-                                    min_cnt = cnt3
+def choose_coin(level, cnt, before, start):
+    global min_cnt
+
+    if min_cnt <= cnt:
+        return
+
+    if level == 3:
+        cnt += calc_distance(*before, *E)
+        min_cnt = cnt
+        return
+    
+    for i in range(start, 10):
+        coin = coin_list[i]
+        if coin:
+            choose_coin(level+1, cnt+calc_distance(*before, *coin), coin, i+1)
+
+
+choose_coin(0, 0, S, 0)
 
 if min_cnt < 22*N:
     print(min_cnt)
