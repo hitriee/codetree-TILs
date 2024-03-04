f142905f682a078fe8b652c633a0f1424f1fc956
나원경 <97578485+hitriee@users.noreply.github.com>
2024-03-04 14:53:15 +0900

    Title: 최소 경로로 탈출 하기, Time: 313ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240304/최소 경로로 탈출 하기/escape-with-min-distance.py
@@ -0,0 +1,30 @@
+n, m = map(int, input().split())
+arr = [input().split() for _ in range(n)]
+visited = [[False] * m for _ in range(n)]
+dy, dx = [1, 0, -1, 0], [0, 1, 0, -1]
+
+def can_move(y, x):
+    if 0 <= y < n and 0 <= x < m and arr[y][x] == '1' and not visited[y][x]:
+        visited[y][x] = True
+        return True
+    return False
+
+def move():
+    from collections import deque
+    
+    q = deque([(0, 0, 0)])
+    while q:
+        y, x, cnt = q.popleft()
+        if y == n-1 and x == m-1:
+            return cnt
+        
+        cnt += 1
+        
+        for i in range(4):
+            ny, nx = y+dy[i], x+dx[i]
+            if can_move(ny, nx):
+                q.append((ny, nx, cnt))
+
+    return -1
+
+print(move())
