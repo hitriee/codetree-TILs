ca9a4a847a85185616c02341694f59a16320f29f
나원경 <97578485+hitriee@users.noreply.github.com>
2024-03-05 12:53:31 +0900

    Title: 나이트, Time: 299ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240305/나이트/knight-movements.py
@@ -0,0 +1,37 @@
+n = int(input())
+r1, c1, r2, c2 = map(lambda x: int(x)-1, input().split())
+visited = [[False] * n for _ in range(n)]
+dy, dx = [], []
+
+for i in (2, 1, -2, -1):
+    for _ in range(2):
+        dy.append(i)
+    dx.append(3-abs(i))
+    dx.append(abs(i)-3)
+
+def can_move(y, x):
+    if 0 <= y < n and 0 <= x < n and not visited[y][x]:
+        visited[y][x] = True
+        return True
+    return False
+
+def move_knight():
+    from collections import deque
+
+    q = deque()
+    q.append((r1, c1, 0))
+    while q:
+        y, x, cnt = q.popleft()
+        if y == r2 and x == c2:
+            return cnt
+        
+        cnt += 1
+        for i in range(8):
+            ny, nx = y+dy[i], x+dx[i]
+            if can_move(ny, nx):
+                q.append((ny, nx, cnt))
+
+    return -1
+
+
+print(move_knight())
