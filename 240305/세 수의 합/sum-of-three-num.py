99450a2c61f03c21bd58f25e45d0917c5e99c6d2
나원경 <97578485+hitriee@users.noreply.github.com>
2024-03-05 22:58:57 +0900

    Title: 세 수의 합, Time: 126ms, Memory: 25MB, Status: Passed - CodeTree

--- 240305/세 수의 합/sum-of-three-num.py
+++ 240305/세 수의 합/sum-of-three-num.py
@@ -1,7 +1,6 @@
 n, k = map(int, input().split())
 numbers = list(map(int, input().split()))
 check = {}
-cnt = 0
 
 for number in numbers:
     if check.get(number):
@@ -12,22 +11,36 @@ for number in numbers:
 sorted_keys = sorted(check)
 m = len(sorted_keys)
 
-for i in range(m):
-    num1 = sorted_keys[i]
-    val1 = check[num1]
-    num2 = k-num1*2
-    if num1 == num2:
-        cnt += ((val1-2)*(val1-1)*val1)//6
-    elif num1 < num2 and check.get(num2):
-        cnt += ((val1-1)*val1)//2 * check[num2]
+def comb(m, r):
+    num1, num2 = m, 1
+    for i in range(1, r):
+        num1 *= (m-i)
+        num2 *= (i+1)
+    
+    return num1//num2
 
-    for j in range(i+1, m):
-        num2 = sorted_keys[j]
-        val2 = check[num2]
-        num3 = k - (num1 + num2)
-        if num3 == num2:
-            cnt += val1 * (((val2-1)*val2)//2)
-        elif num2 < num3 and check.get(num3):
-            cnt += val1 * val2 * check[num3]
-        
-print(cnt)
+
+def cnt_comb():
+    cnt = 0
+
+    for i in range(m):
+        num1 = sorted_keys[i]
+        val1 = check[num1]
+        num2 = k-num1*2
+        if num1 == num2:
+            cnt += comb(val1, 3)
+        elif num1 < num2 and check.get(num2):
+            cnt += comb(val1, 2) * check[num2]
+
+        for j in range(i+1, m):
+            num2 = sorted_keys[j]
+            val2 = check[num2]
+            num3 = k - (num1 + num2)
+            if num3 == num2:
+                cnt += val1 * comb(val2, 2)
+            elif num2 < num3 and check.get(num3):
+                cnt += val1 * val2 * check[num3]
+            
+    return cnt
+
+print(cnt_comb())
