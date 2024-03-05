67beb7944ef75c9a310abf2141276cd368e101b1
나원경 <97578485+hitriee@users.noreply.github.com>
2024-03-05 22:45:22 +0900

    Title: 세 수의 합, Time: 111ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240305/세 수의 합/sum-of-three-num.py
+++ 240305/세 수의 합/sum-of-three-num.py
@@ -12,15 +12,15 @@ for number in numbers:
 sorted_keys = sorted(check)
 m = len(sorted_keys)
 
-for i in range(m-2):
+for i in range(m):
     num1 = sorted_keys[i]
     val1 = check[num1]
     if num1*3 == k:
-        cnt += ((val1-2)*(val1-1)*val1)//3
+        cnt += ((val1-2)*(val1-1)*val1)//6
     elif check.get(k-num1*2):
         cnt += check[k-num1*2]
 
-    for j in range(i+1, m-1):
+    for j in range(i+1, m):
         num2 = sorted_keys[j]
         val2 = check[num2]
         num3 = k - (num1 + num2)
