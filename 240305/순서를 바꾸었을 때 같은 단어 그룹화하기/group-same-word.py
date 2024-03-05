673676343273c76bc25f68c373656ae4c666a455
나원경 <97578485+hitriee@users.noreply.github.com>
2024-03-05 22:19:44 +0900

    Title: 순서를 바꾸었을 때 같은 단어 그룹화하기, Time: 326ms, Memory: 25MB, Status: Passed - CodeTree

--- 240305/순서를 바꾸었을 때 같은 단어 그룹화하기/group-same-word.py
+++ 240305/순서를 바꾸었을 때 같은 단어 그룹화하기/group-same-word.py
@@ -1,34 +1,20 @@
 n = int(input())
-words = [input() for _ in range(n)]
-finished = [False] * n
-max_cnt = 0
-for i in range(n-1):
-    if not finished[i]:
-        finished[i] = True
-        cnt = 1
-        check = {}
-        for alp in words[i]:
-            if check.get(alp):
-                check[alp] += 1
-            else:
-                check[alp] = 1
-        
-        for j in range(i+1, n):
-            if not finished[j]:
-                temp = dict(check)
-                for alp in words[j]:
-                    if temp.get(alp):
-                        temp[alp] -= 1
-                        if temp[alp] == 0:
-                            del temp[alp]
-                    else:
-                        break
-                else:
-                    if not temp:
-                        cnt += 1
-                        finished[j] = True
+words = [''.join(sorted(input())) for _ in range(n)]
+words.sort()
 
-        if max_cnt < cnt:
-            max_cnt = cnt
+i, max_cnt = 0, 1
+while i < n-1:
+    cnt = 1
+    word = words[i]
+    j = i+1
+    while word == words[j]:
+        j += 1
+        cnt += 1
+        if j == n:
+            break
+    i = j
+            
+    if max_cnt < cnt:
+        max_cnt = cnt
 
 print(max_cnt)
