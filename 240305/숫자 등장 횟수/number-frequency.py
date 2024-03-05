e7c2139369eecce4f5dc8ee9b1da7cba31071bd6
나원경 <97578485+hitriee@users.noreply.github.com>
2024-03-05 21:03:53 +0900

    Title: 숫자 등장 횟수, Time: 210ms, Memory: 33MB, Status: Passed - CodeTree

--- 240305/숫자 등장 횟수/number-frequency.py
+++ 240305/숫자 등장 횟수/number-frequency.py
@@ -1,10 +1,12 @@
 n, m = map(int, input().split())
 arr = input().split()
-query_tuple = input().split()
+query_list = input().split()
 cnt_frequency = {}
 
 for num in arr:
     cnt_frequency[num] = cnt_frequency.get(num, 0) + 1
 
-for num in query_tuple:
-    print(cnt_frequency.get(num, 0), end=' ')
+for i in range(m):
+    query_list[i] = str(cnt_frequency.get(query_list[i], 0))
+
+print(' '.join(query_list))
