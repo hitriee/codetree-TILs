3224c13c9dd33201e76cb81325bf506cfdaeae2e
나원경 <97578485+hitriee@users.noreply.github.com>
2024-03-05 21:41:03 +0900

    Title: 두 수의 합, Time: 172ms, Memory: 36MB, Status: Passed - CodeTree

---
+++ 240305/두 수의 합/sum-of-two-num.py
@@ -0,0 +1,23 @@
+n, k = map(int, input().split())
+numbers = list(map(int, input().split()))
+half, cnt = k//2, 0, 
+check = {}
+
+for number in numbers:
+    if check.get(number):
+        check[number] += 1
+    else:
+        check[number] = 1
+
+for number in numbers:
+    if number < half:
+        if check.get(k-number):
+            cnt += check[k-number]
+
+if check.get(half):
+    if k % 2 == 0:
+        cnt += ((check[half] - 1) * check[half]) // 2
+    elif check.get(k-half):
+        cnt += check[k-half]
+
+print(cnt)
