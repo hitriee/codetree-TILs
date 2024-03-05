e6eb0c178d81b2ff77e268bcc52962d2bd87f43d
나원경 <97578485+hitriee@users.noreply.github.com>
2024-03-06 08:57:53 +0900

    Title: hashset 기본, Time: 466ms, Memory: 28MB, Status: Passed - CodeTree

--- 240306/hashset 기본/hashset-basic.py
+++ 240306/hashset 기본/hashset-basic.py
@@ -1,10 +1,13 @@
 n = int(input())
 hashset = set()
 for _ in range(n):
-    command = input().split()
-    if command[0] == 'add':
-        hashset.add(command[1])
-    elif command[0] == 'remove':
-        hashset.remove(command[1])
-    else:
-        print('true' if command[1] in hashset else 'false')
+    command, num = input().split()
+    if num not in hashset:
+        if command == 'add':
+            hashset.add(num)
+        else:
+            print('false')
+    elif command == 'remove':
+        hashset.remove(num)
+    elif command == 'find':
+        print('true')
