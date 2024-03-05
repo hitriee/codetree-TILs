d922aa1a8f455c3738b1f5bb658a6f87e8fde622
나원경 <97578485+hitriee@users.noreply.github.com>
2024-03-05 15:47:06 +0900

    Title: 안전 지대, Time: 803ms, Memory: 86MB, Status: Passed - CodeTree

--- 240305/안전 지대/comfort-zone.py
+++ 240305/안전 지대/comfort-zone.py
@@ -1,12 +1,17 @@
+from sys import setrecursionlimit
+
+setrecursionlimit(5000)
+
 N, M = map(int, input().split())
 town = [list(map(int, input().split())) for _ in range(N)]
 dy, dx = [1, 0, -1, 0], [0, 1, 0, -1]
 max_safe_cnt = limit = 0
 
 def possible(y, x, ref):
-    if 0 <= y < N and 0 <= x < M and town[y][x] > ref and not visited[y][x]:
+    if 0 <= y < N and 0 <= x < M and not visited[y][x]:
         visited[y][x] = True
-        return True
+        if town[y][x] > k:
+            return True
     return False
 
 def cnt_safe_area(y, x, ref):
@@ -27,10 +32,11 @@ for k in range(1, limit):
     cnt = 0
     for i in range(N):
         for j in range(M):
-            if town[i][j] > k and not visited[i][j]:
+            if not visited[i][j]:
                 visited[i][j] = True
-                cnt += 1 
-                cnt_safe_area(i, j, k)
+                if town[i][j] > k:
+                    cnt += 1 
+                    cnt_safe_area(i, j, k)
 
     if max_safe_cnt < cnt:
         max_safe_cnt = cnt
