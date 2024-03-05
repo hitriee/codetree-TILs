3643b010122ed0cafa25d0683af123378bbb8aa7
나원경 <97578485+hitriee@users.noreply.github.com>
2024-03-05 11:14:04 +0900

    Title: 안전 지대, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240305/안전 지대/comfort-zone.py
+++ 240305/안전 지대/comfort-zone.py
@@ -1,7 +1,7 @@
 N, M = map(int, input().split())
 town = [list(map(int, input().split())) for _ in range(N)]
 dy, dx = [1, 0, -1, 0], [0, 1, 0, -1]
-max_safe_cnt = height = limit = 0
+max_safe_cnt = limit = 0
 
 def possible(y, x, ref):
     if 0 <= y < N and 0 <= x < M and town[y][x] > ref and not visited[y][x]:
@@ -19,6 +19,8 @@ for i in range(N):
     for j in range(M):
         if town[i][j] > limit:
             limit = town[i][j]
+
+height = limit
 
 for k in range(1, limit):
     visited = [[False] * M for _ in range(N)]
