88d4e72a67c98be6bd7c41b0cb8270d0ccbb35e5
나원경 <97578485+hitriee@users.noreply.github.com>
2024-03-06 10:39:57 +0900

    Title: 가장 가까운 점, Time: 641ms, Memory: 32MB, Status: Passed - CodeTree

--- 240306/가장 가까운 점/nearest-point.py
+++ 240306/가장 가까운 점/nearest-point.py
@@ -1,14 +1,15 @@
 from heapq import heappush, heappop
 n, m = map(int, input().split())
 dots = []
+
 for _ in range(n):
     x, y = map(int, input().split())
-    heappush(dots, (abs(x)+abs(y), x, y))
+    heappush(dots, (x+y, x, y))
 
 for _ in range(m):
     _, x, y = heappop(dots)
     x += 2
     y += 2
-    heappush(dots, (abs(x)+abs(y), x, y))
+    heappush(dots, (x+y, x, y))
 
 print(*dots[0][1:])
