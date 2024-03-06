f8cfbca60747ee56841dbca057efe2f02a6fe545
나원경 <97578485+hitriee@users.noreply.github.com>
2024-03-06 09:54:30 +0900

    Title: C-TAG, Time: 2784ms, Memory: 26MB, Status: Passed - CodeTree

--- 240306/C-TAG/c-tag.py
+++ 240306/C-TAG/c-tag.py
@@ -1,33 +1,28 @@
 n, m = map(int, input().split())
 a = [input() for _ in range(n)]
 b = [input() for _ in range(n)]
-path = []
+a_comb, b_comb = [[''] * 3 for _ in range(n)], [['']*3 for _ in range(n)]
 cnt = 0
 
 def find_comb(level, start):
     global cnt
 
     if level == 3:
-        a_comb, b_comb = set(), set()
-        for i in range(n):
-            a_result = b_result = ''
-            for j in path:
-                a_result += a[i][j]
-                b_result += b[i][j]
-            a_comb.add(a_result)
-            b_comb.add(b_result)
+        a_set = set(''.join(a_comb[i]) for i in range(n))
+        b_set = set(''.join(b_comb[i]) for i in range(n))
         
-        for a_str in a_comb:
-            if a_str in b_comb:
+        for a_str in a_set:
+            if a_str in b_set:
                 break
         else:
             cnt += 1
         return
     
     for i in range(start, m):
-        path.append(i)
+        for j in range(n):
+            a_comb[j][level] = a[j][i]
+            b_comb[j][level] = b[j][i]
         find_comb(level+1, i+1)
-        path.pop()
 
 find_comb(0, 0)
 print(cnt)
