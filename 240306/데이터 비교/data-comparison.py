d56d7086edf78b10168e02451ecdf8f398a4b860
나원경 <97578485+hitriee@users.noreply.github.com>
2024-03-06 09:03:15 +0900

    Title: 데이터 비교, Time: 1413ms, Memory: 73MB, Status: Passed - CodeTree

--- 240306/데이터 비교/data-comparison.py
+++ 240306/데이터 비교/data-comparison.py
@@ -2,8 +2,9 @@ n = int(input())
 set1 = set(input().split())
 m = int(input())
 arr = input().split()
+result = ''
 
 for i in range(m):
-    arr[i] = '1' if arr[i] in set1 else '0'
+    result += '1 ' if arr[i] in set1 else '0 '
 
-print(' '.join(arr))
+print(result)
