084b16561a3fabda6f512e2cfcc33dee9e7706a6
나원경 <97578485+hitriee@users.noreply.github.com>
2024-03-06 20:26:31 +0900

    Title: 마지막으로 남은 숫자, Time: 320ms, Memory: 30MB, Status: Passed - CodeTree

--- 240306/마지막으로 남은 숫자/last-remaining-number.py
+++ 240306/마지막으로 남은 숫자/last-remaining-number.py
@@ -4,13 +4,10 @@ n = int(input())
 numbers = list(map(lambda x: -int(x), input().split()))
 heapify(numbers)
 
-while n >= 2:
+while len(numbers) >= 2:
     number1 = heappop(numbers)
     number2 = heappop(numbers)
     if number1 != number2:
         heappush(numbers, -abs(number1 - number2))
-        n -= 1
-    else:
-        n -= 2
 
-print(-numbers[0] if n == 1 else -1)
+print(-numbers[0] if len(numbers) == 1 else -1)
