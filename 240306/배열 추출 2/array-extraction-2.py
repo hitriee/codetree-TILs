48111c72ee638e68237b1dc124fed0d02d2d33a2
나원경 <97578485+hitriee@users.noreply.github.com>
2024-03-06 20:45:44 +0900

    Title: 배열 추출 2, Time: 505ms, Memory: 30MB, Status: Passed - CodeTree

--- 240306/배열 추출 2/array-extraction-2.py
+++ 240306/배열 추출 2/array-extraction-2.py
@@ -5,10 +5,12 @@ abs_heap = []
 n = int(input())
 for _ in range(n):
     x = int(input())
-    if x:
-        heappush(abs_heap, (abs(x), x))
+    if x > 0:
+        heappush(abs_heap, (x, x))
+    elif x < 0:
+        heappush(abs_heap, (-x, x))
     elif abs_heap:
-        _, min_num = heappop(abs_heap)
+        min_num = heappop(abs_heap)[1]
         print(min_num)
     else:
         print(0)
