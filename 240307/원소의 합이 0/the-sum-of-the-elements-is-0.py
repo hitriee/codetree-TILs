90a4b9e4d87b9848a7348c4e8ba3d577d2369043
나원경 <97578485+hitriee@users.noreply.github.com>
2024-03-07 22:59:49 +0900

    Title: 원소의 합이 0, Time: 202ms, Memory: 39MB, Status: Passed - CodeTree

--- 240307/원소의 합이 0/the-sum-of-the-elements-is-0.py
+++ 240307/원소의 합이 0/the-sum-of-the-elements-is-0.py
@@ -1,21 +1,25 @@
 n = int(input())
-sum_dict_list = [{} for _ in range(2)]
+sum_dict = {}
 cnt = 0
 
-for i in range(2):
-    A = list(map(int, input().split()))
-    B = list(map(int, input().split()))
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+C = list(map(int, input().split()))
+D = list(map(int, input().split()))
 
-    for num1 in A:
-        for num2 in B:
-            sum_val = num1 + num2
-            if sum_dict_list[i].get(sum_val):
-                sum_dict_list[i][sum_val] += 1
-            else:
-                sum_dict_list[i][sum_val] = 1
 
-for key in sum_dict_list[0]:
-    if sum_dict_list[1].get(-key):
-        cnt += sum_dict_list[0][key] * sum_dict_list[1][-key]
+for num1 in A:
+    for num2 in B:
+        sum_val = num1 + num2
+        if sum_dict.get(sum_val):
+            sum_dict[sum_val] += 1
+        else:
+            sum_dict[sum_val] = 1
+
+for num1 in C:
+    for num2 in D:
+        minus_sum = -(num1 + num2)
+        if sum_dict.get(minus_sum):
+            cnt += sum_dict[minus_sum]
 
 print(cnt)
