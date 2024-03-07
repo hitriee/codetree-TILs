cf4511de5df13601f5450423fdab5bd1e52ac358
나원경 <97578485+hitriee@users.noreply.github.com>
2024-03-07 20:19:32 +0900

    Title: 중앙값, Time: 415ms, Memory: 37MB, Status: Passed - CodeTree

--- 240307/중앙값/median.py
+++ 240307/중앙값/median.py
@@ -7,18 +7,22 @@ for _ in range(t):
     result = numbers[:1]
     mid_num = result[0]
     smaller, larger = [], []
+    
     for i in range(1, m):
         number = numbers[i]
         if number >= mid_num:
             heappush(larger, number)
         else:
             heappush(smaller, -number)
+        
         if i%2 == 0:
             len_l, len_s = len(larger), len(smaller)
+            
             if len_l > len_s:
                 for _ in range((len_l - len_s)//2):
                     heappush(smaller, -mid_num)
                     mid_num = heappop(larger)
+            
             elif len_l < len_s:
                 for _ in range((len_s - len_l)//2):
                     heappush(larger, mid_num)
