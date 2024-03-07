444f82022a6a51bb39bd9d051ad52ff8876233bd
나원경 <97578485+hitriee@users.noreply.github.com>
2024-03-07 20:45:56 +0900

    Title: 정원 입장은 선착순, Time: 1031ms, Memory: 39MB, Status: Passed - CodeTree

---
+++ 240307/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.py
@@ -0,0 +1,29 @@
+from sys import stdin
+from heapq import heappush, heappop
+
+new_input = stdin.readline
+N = int(new_input())
+people = []
+for i in range(N):
+    start, duration = map(int, new_input().split())
+    heappush(people, (start, i, duration))
+
+tags = []
+max_delay = 0
+while people or tags:
+    if tags:
+        i, start, duration = heappop(tags)
+        delay = end - start
+        if max_delay < delay:
+            max_delay = delay
+        end += duration
+    else:
+        start, i, duration = heappop(people)
+        end = start + duration
+
+    
+    while people and people[0][0] < end:
+        s, j, d = heappop(people)
+        heappush(tags, (j, s, d))
+
+print(max_delay)
