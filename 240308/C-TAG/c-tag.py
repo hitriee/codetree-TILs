6b0f591ac540924d28d703e1d0400c84c5202be5
나원경 <97578485+hitriee@users.noreply.github.com>
2024-03-08 11:23:18 +0900

    Title: C-TAG, Time: 2160ms, Memory: 26MB, Status: Passed - CodeTree

--- 240308/C-TAG/c-tag.py
+++ 240308/C-TAG/c-tag.py
@@ -1,27 +1,36 @@
-n, m = map(int, input().split())
-a = [input() for _ in range(n)]
-b = [input() for _ in range(n)]
-a_comb, b_comb = [[''] * 3 for _ in range(n)], [['']*3 for _ in range(n)]
+from sys import stdin
+
+new_input = stdin.readline
+n, m = map(int, new_input().split())
+a = [new_input().rstrip() for _ in range(n)]
+b = [new_input().rstrip() for _ in range(n)]
+path = []
 cnt = 0
 
 def find_comb(level, start):
     global cnt
 
     if level == 3:
-        a_set = set(''.join(a_comb[i]) for i in range(n))
-        b_set = set(''.join(b_comb[i]) for i in range(n))
+        a_comb, b_comb = set(), set()
+        for i in range(n):
+            a_result = b_result = ''
+            for j in path:
+                a_result += a[i][j]
+                b_result += b[i][j]
+            a_comb.add(a_result)
+            b_comb.add(b_result)
         
-        for a_str in a_set:
-            if a_str in b_set:
-                return
-        cnt += 1
+        for a_str in a_comb:
+            if a_str in b_comb:
+                break
+        else:
+            cnt += 1
         return
     
     for i in range(start, m):
-        for j in range(n):
-            a_comb[j][level] = a[j][i]
-            b_comb[j][level] = b[j][i]
+        path.append(i)
         find_comb(level+1, i+1)
+        path.pop()
 
 find_comb(0, 0)
 print(cnt)
