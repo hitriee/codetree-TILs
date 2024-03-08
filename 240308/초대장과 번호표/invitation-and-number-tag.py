0aaa57b0d1170c813a2d9034aa7da9d89f71b7f7
나원경 <97578485+hitriee@users.noreply.github.com>
2024-03-08 11:11:22 +0900

    Title: 초대장과 번호표, Time: 1059ms, Memory: 44MB, Status: Passed - CodeTree

--- 240308/초대장과 번호표/invitation-and-number-tag.py
+++ 240308/초대장과 번호표/invitation-and-number-tag.py
@@ -1,29 +1,28 @@
 from collections import deque
 
-N, G = map(int, input().split())
+_, G = map(int, input().split())
 invited = {1}
-groups = []
-people = [[] for _ in range(N+1)]
-for i in range(G):
-    headcount, *group = tuple(map(int, input().split()))
+groups = deque()
+for _ in range(G):
+    _, *group = map(int, input().split())
     groups.append(set(group))
-    for j in group:
-        people[j].append(i)
 
-removed = set()
-q = deque(invited)
+removed = [False] * G
 
-while q:
-    num = q.popleft()
-    for i in people[num]:
-        if i not in removed:
-            not_invited = groups[i] - invited
-            if len(not_invited) == 0:
-                removed.add(i)
-            elif len(not_invited) == 1:
-                invited.update(not_invited)
-                q.extend(not_invited)
-                removed.add(i)
 
+while True:
+    changed = False
+    for _ in range(G):
+        group = groups.popleft()
+        not_invited = set(group) - invited
+        if len(not_invited) <= 1:
+            G -= 1
+            invited.update(not_invited)
+            changed = True
+        else:
+            groups.append(group)
+        
+    if not changed:
+        break
 
 print(len(invited))
