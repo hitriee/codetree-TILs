46459908fee6a11d712df399f9b1f10ae3acb5ed
나원경 <97578485+hitriee@users.noreply.github.com>
2024-03-08 11:21:23 +0900

    Title: C-TAG, Time: 2855ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240308/C-TAG/c-tag.py
@@ -0,0 +1,27 @@
+n, m = map(int, input().split())
+a = [input() for _ in range(n)]
+b = [input() for _ in range(n)]
+a_comb, b_comb = [[''] * 3 for _ in range(n)], [['']*3 for _ in range(n)]
+cnt = 0
+
+def find_comb(level, start):
+    global cnt
+
+    if level == 3:
+        a_set = set(''.join(a_comb[i]) for i in range(n))
+        b_set = set(''.join(b_comb[i]) for i in range(n))
+        
+        for a_str in a_set:
+            if a_str in b_set:
+                return
+        cnt += 1
+        return
+    
+    for i in range(start, m):
+        for j in range(n):
+            a_comb[j][level] = a[j][i]
+            b_comb[j][level] = b[j][i]
+        find_comb(level+1, i+1)
+
+find_comb(0, 0)
+print(cnt)
