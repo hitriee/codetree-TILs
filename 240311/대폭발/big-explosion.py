d8c67f7d233b50c0ac6ed5d6da752d45957d75f3
나원경 <97578485+hitriee@users.noreply.github.com>
2024-03-11 22:47:00 +0900

    Title: 대폭발, Time: 124ms, Memory: 25MB, Status: Passed - CodeTree

--- 240311/대폭발/big-explosion.py
+++ 240311/대폭발/big-explosion.py
@@ -1,10 +1,12 @@
 n, m, r, c = map(int, input().split())
-arr = [[False] * (n+1) for _ in range(n+1)]
+r -= 1
+c -= 1
+arr = [[False] * n for _ in range(n)]
 dy, dx = [-1, 0, 1, 0], [0, -1, 0, 1]
 arr[r][c] = True
 
 def can_add_dynamite(y, x):
-    if 0 < y <= n and 0 < x <= n and not arr[y][x]:
+    if 0 <= y < n and 0 <= x < n and not arr[y][x]:
         arr[y][x] = True
         return True
     return False
