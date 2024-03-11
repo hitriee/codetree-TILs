87bf83cdb2ea0ec35cc61e52fa56b64651a9ddca
나원경 <97578485+hitriee@users.noreply.github.com>
2024-03-11 22:46:10 +0900

    Title: 대폭발, Time: 165ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240311/대폭발/big-explosion.py
@@ -0,0 +1,28 @@
+n, m, r, c = map(int, input().split())
+arr = [[False] * (n+1) for _ in range(n+1)]
+dy, dx = [-1, 0, 1, 0], [0, -1, 0, 1]
+arr[r][c] = True
+
+def can_add_dynamite(y, x):
+    if 0 < y <= n and 0 < x <= n and not arr[y][x]:
+        arr[y][x] = True
+        return True
+    return False
+
+def cnt_dynamite():
+    cnt = distance = 1
+    now = [(r, c)]
+    for _ in range(m):
+        before = now[:]
+        for y, x in before:
+            for i in range(4):
+                ny, nx = y+dy[i]*distance, x+dx[i]*distance
+                if can_add_dynamite(ny, nx):
+                    now.append((ny, nx))
+                    cnt += 1
+                
+        distance *= 2
+    
+    return cnt
+
+print(cnt_dynamite())
