acdcd87c6349f464bb77a964800908736c4eb37e
나원경 <97578485+hitriee@users.noreply.github.com>
2024-03-12 16:35:08 +0900

    Title: 단 한 번의 2048 시도, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240312/단 한 번의 2048 시도/one-trial-of-2048-game.py
@@ -0,0 +1,93 @@
+arr = [list(map(int, input().split())) for _ in range(4)]
+direction = input()
+if direction == 'U':
+    for j in range(4):
+        temp1, temp2 = [], []
+        for i in range(4):
+            if arr[i][j]:
+                temp1.append(arr[i][j])
+        
+        for i in range(len(temp1)-1):
+            if temp1[i]:
+                if temp1[i] == temp1[i+1]:
+                    temp2.append(temp1[i] * 2)
+                    temp1[i+1] = 0
+                else:
+                    temp2.append(temp1[i])
+        
+        if temp1[-1]:
+            temp2.append(temp1[-1])
+
+        for i in range(len(temp2)):
+            arr[i][j] = temp2[i]
+        for i in range(len(temp2), 4):
+            arr[i][j] = 0
+
+elif direction == 'D':
+    for j in range(4):
+        temp1, temp2 = [], []
+        for i in range(4):
+            if arr[i][j]:
+                temp1.append(arr[i][j])
+        
+        for i in range(len(temp1)-1, 0, -1):
+            if temp1[i]:
+                if temp1[i] == temp1[i-1]:
+                    temp2.append(temp1[i] * 2)
+                    temp1[i-1] = 0
+                else:
+                    temp2.append(temp1[i])
+        
+        if temp1[0]:
+            temp2.append(temp1[0])
+
+        for i in range(3, 3-len(temp2), -1):
+            arr[i][j] = temp2[3-i]
+        
+        for i in range(3-len(temp2)+1):
+            arr[i][j] = 0
+        
+elif direction == 'R':
+    for i in range(4):
+        temp1, temp2 = [], []
+        for j in range(4):
+            if arr[i][j]:
+                temp1.append(arr[i][j])
+        
+        for j in range(len(temp1)-1, 0, -1):
+            if temp1[j]:
+                if temp1[j] == temp1[j-1]:
+                    temp2.append(temp1[j] * 2)
+                    temp1[j-1] = 0
+                else:
+                    temp2.append(temp1[j])
+        
+        if temp1[0]:
+            temp2.append(temp1[0])
+        
+        arr[i] = [0] * (4 - len(temp2)) + temp2[::-1]
+
+                
+
+else:
+    for i in range(4):
+        temp1, temp2 = [], []
+        for j in range(4):
+            if arr[i][j]:
+                temp1.append(arr[i][j])
+        
+        for j in range(len(temp1)-1):
+            if temp1[j]:
+                if temp1[j] == temp1[j+1]:
+                    temp2.append(temp1[j] * 2)
+                    temp1[j+1] = 0
+                else:
+                    temp2.append(temp1[j])
+        
+        if temp1[-1]:
+            temp2.append(temp1[-1])
+        
+        arr[i] = temp2 + [0] * (4 - len(temp2))
+
+for i in range(4):
+    print(*arr[i])
