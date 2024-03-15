05fc4db653d378c12b80db2941be84ba0b7a9bb2
나원경 <97578485+hitriee@users.noreply.github.com>
2024-03-15 11:32:48 +0900

    Title: 괄호를 빨강으로 칠하기, Time: 5036ms, Memory: 29MB, Status: Time Limit Exceed - CodeTree

--- 240315/괄호를 빨강으로 칠하기/draw-brackets-in-red.py
+++ 240315/괄호를 빨강으로 칠하기/draw-brackets-in-red.py
@@ -1,25 +1,31 @@
 input_val = input()
 n = len(input_val)
-cnt = 0
+cnt = close_cnt = 0
 
-def cnt_correct(level, red, blue):
+for element in input_val:
+    if element == ')':
+        close_cnt += 1
+
+def cnt_correct(level, red, blue, left):
     global cnt
 
+    if red > left or blue > left:
+        return
+
     if level == n:
-        if red == 0 and blue == 0:
-            cnt += 1
+        cnt += 1
         return
     
     element = input_val[level]
     if element == '(':
-        cnt_correct(level+1, red+1, blue)
-        cnt_correct(level+1, red, blue+1)
+        cnt_correct(level+1, red+1, blue, left)
+        cnt_correct(level+1, red, blue+1, left)
     else:
         if red > 0:
-            cnt_correct(level+1, red-1, blue)
+            cnt_correct(level+1, red-1, blue, left-1)
         if blue > 0:
-            cnt_correct(level+1, red, blue-1)
+            cnt_correct(level+1, red, blue-1, left-1)
 
-cnt_correct(0, 0, 0)
+cnt_correct(0, 0, 0, close_cnt)
 
 print(cnt%2012)
