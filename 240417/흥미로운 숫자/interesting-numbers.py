59223ad392f406121a87d83743c71b2964c1d184
나원경 <97578485+hitriee@users.noreply.github.com>
2024-04-17 13:15:51 +0900

    Title: 흥미로운 숫자, Time: 5084ms, Memory: 25MB, Status: Time Limit Exceed - CodeTree

--- 240417/흥미로운 숫자/interesting-numbers.py
+++ 240417/흥미로운 숫자/interesting-numbers.py
@@ -3,10 +3,16 @@ cnt = 0
 for i in range(X, Y+1):
     str_num = str(i)
     num_dict = {}
+    length = 0
     for element in str_num:
-        num_dict[element] = num_dict.get(element, 0) + 1
-    
-    if len(num_dict) == 2:
+        if num_dict.get(element):
+            num_dict[element] += 1
+        elif length < 2:
+            num_dict[element] = 1
+            length += 1
+        else:
+            break
+    else:
         one = num_dict[str_num[0]]
         if one == 1 or one == len(str_num) - 1:
             cnt += 1
