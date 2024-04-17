ff0dfaac103384c24cc18c00a94dea4c0cc00c4c
나원경 <97578485+hitriee@users.noreply.github.com>
2024-04-17 13:50:15 +0900

    Title: 흥미로운 숫자, Time: 135ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240417/흥미로운 숫자/interesting-numbers.py
+++ 240417/흥미로운 숫자/interesting-numbers.py
@@ -1,20 +1,37 @@
 X, Y = map(int, input().split())
-cnt = 0
-for i in range(X, Y+1):
-    str_num = str(i)
-    num_dict = {}
-    length = 0
-    for element in str_num:
-        if num_dict.get(element):
-            num_dict[element] += 1
-        elif length < 2:
-            num_dict[element] = 1
-            length += 1
-        else:
-            break
-    else:
-        one = num_dict[str_num[0]]
-        if one == 1 or one == len(str_num) - 1:
+cnt = dict_len = 0
+num_dict = {i: 0 for i in range(10)}
+min_len, max_len = len(str(X)), len(str(Y))
+
+
+def back_tracking(num, length, dict_len):
+    global cnt
+
+    if length > max_len:
+        return
+    if length >= min_len:
+        int_num = int(num)
+        if int_num < X:
+            return
+        if int_num > Y:
+            return
+        if dict_len == 2:
             cnt += 1
+
+    for i in range(10):
+        if num_dict[i]:
+            num_dict[i] += 1
+            back_tracking(num + str(i), length + 1, dict_len)
+            num_dict[i] -= 1
+        elif dict_len == 1:
+            num_dict[i] = 1
+            back_tracking(num + str(i), length + 1, dict_len + 1)
+            num_dict[i] = 0
+
+
+for i in range(1, 10):
+    num_dict[i] += 1
+    back_tracking(str(i), 1, 1)
+    num_dict[i] -= 1
 
 print(cnt)
