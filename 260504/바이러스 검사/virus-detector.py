a09daa7130dae52edc88ccd294f2385964c9eb0a
나원경 <97578485+hitriee@users.noreply.github.com>
2026-05-04 20:46:17 +0900

    [Passed] 바이러스 검사 (294ms, 108MB)

--- 260504/바이러스 검사/virus-detector.py
+++ 260504/바이러스 검사/virus-detector.py
@@ -2,11 +2,10 @@ from math import ceil
 
 n = int(input())
 customers = list(map(int, input().split()))
-leader, follower = list(map(int, input().split()))
-cnt = 0
+leader, follower = map(int, input().split())
+cnt = n
 
 for customer in customers:
-    cnt += 1
     if customer > leader:
         cnt += ceil((customer - leader) / follower)
 
