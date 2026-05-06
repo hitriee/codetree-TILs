d607d28c47b7c1c7022ef0160478eb2aaff55c3b
나원경 <97578485+hitriee@users.noreply.github.com>
2026-05-06 15:04:21 +0900

    [Passed] 외주 수익 최대화하기 (44ms, 16MB)

--- samsung-sw/외주 수익 최대화하기/max-of-outsourcing-profit.py
+++ samsung-sw/외주 수익 최대화하기/max-of-outsourcing-profit.py
@@ -1,11 +1,11 @@
 n = int(input())
-max_income = [0] * (n+1)
+max_income = [0] * (n+2)
 
 for i in range(n):
     t, p = map(int, input().split())
-    max_income[i] = max(max_income[:i+1])
+    max_income[i+1] = max(max_income[i:i+2])
     if i+t < n+1:
-        max_income[i+t] = max(max_income[i] + p, max_income[i+t])
+        max_income[i+t+1] = max(max_income[i+1] + p, max_income[i+t+1])
 
 
 print(max(max_income[-2:]))
