89f02577363772a23c9701f303b96fbc6e1ab8a0
나원경 <97578485+hitriee@users.noreply.github.com>
2026-05-07 13:07:29 +0900

    [Passed] 조삼모사 (380ms, 35MB)

--- samsung-sw/조삼모사/three-at-dawn-and-four-at-dusk.py
+++ samsung-sw/조삼모사/three-at-dawn-and-four-at-dusk.py
@@ -2,33 +2,43 @@ n = int(input())
 table = [list(map(int, input().split())) for _ in range(n)]
 min_dif = 1000
 half = n // 2
+now = 0
 path = []
+visited = [False] * (1<<20)
 
-def backtracking(level, start, score):
-    global min_dif
+def backtracking(level, start, score1):
+    global min_dif, now
 
     if level == half:
+        if visited[now]:
+            return
+        
         new_path = []
-        idx = remain = 0
+        remain = score2 = 0
         for i in range(n):
-            if idx >= half or path[idx] != i:
+            state = 1 << i
+            if now & state == 0:
                 for j in new_path:
-                    remain += table[i][j] + table[j][i]
+                    score2 += table[i][j] + table[j][i]
+                remain += state
                 new_path.append(i)
-            else:
-                idx += 1
-        dif = abs(score - remain)
+                
+        
+        visited[now] = visited[remain] = True
+        dif = abs(score1 - score2)
         if dif < min_dif:
             min_dif = dif
 
         return
     
     for i in range(start, n):
-        new_score = score
+        new_score = score1
         for j in path:
             new_score += table[i][j] + table[j][i]
+        now += 1 << i
         path.append(i)
         backtracking(level+1, i+1, new_score)
+        now -= 1 << i
         path.pop()
         
     
