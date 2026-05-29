95de5c266333813560f46344a1c4a483b6d72bc9
나원경 <97578485+hitriee@users.noreply.github.com>
2026-05-29 13:35:41 +0900

    [Passed] 막대기 나누기 (52ms, 17MB)

--- trail4/막대기 나누기/rod-cutting.py
+++ trail4/막대기 나누기/rod-cutting.py
@@ -2,14 +2,11 @@ n = int(input())
 profit = [0] + list(map(int, input().split()))
 
 # Please write your code here.
-max_profit = [0] * (n+1)
-for i in range(1, n+1):
-    max_profit[i] = profit[i]
 
 for j in range(1, n+1):
     value = profit[j]
     for i in range(j, n+1):
-        if max_profit[i-j] != 0:
-            max_profit[i] = max(max_profit[i], max_profit[j] + max_profit[i-j])
+        if profit[i-j] != 0:
+            profit[i] = max(profit[i], profit[j] + profit[i-j])
 
-print(max_profit[-1])
+print(profit[-1])
