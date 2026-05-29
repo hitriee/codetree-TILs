72cd1ae8ee3a8c91a93acb2c1d76412e56d45743
나원경 <97578485+hitriee@users.noreply.github.com>
2026-05-29 14:03:06 +0900

    [Passed] 특정 조건에 맞게 K개 중에 1개를 N번 뽑기 (268ms, 27MB)

---
+++ trail4/특정 조건에 맞게 K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition-under-constraint.py
@@ -0,0 +1,25 @@
+K, N = map(int, input().split())
+
+# Please write your code here.
+path = []
+def backtracking(level, cnt):
+    if cnt >= 3:
+        return
+    if level == N:
+        print(*path)
+        return
+    
+    new_level = level+1
+    
+    for i in range(1, K+1):
+        if level == 0 or path[-1] != i:
+            new_cnt = 1
+        else:
+            new_cnt = cnt + 1
+
+        path.append(i)
+        backtracking(new_level, new_cnt)
+        path.pop()
+
+
+backtracking(0, 1)
