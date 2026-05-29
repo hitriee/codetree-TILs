859fa145e2e45d4404bb4e1ab759320f13798a08
나원경 <97578485+hitriee@users.noreply.github.com>
2026-05-29 12:39:15 +0900

    [Passed] 1, 2, 5 더하기 (50ms, 17MB)

--- trail4/1, 2, 5 더하기/1-2-5-plus.py
+++ trail4/1, 2, 5 더하기/1-2-5-plus.py
@@ -4,10 +4,14 @@ n = int(input())
 cnt = [0] * (n+1)
 cnt[0] = 1
 div_num = 10007
-
-for i in range(1, n+1):
+for i in range(1, 5):
     for di in (1, 2, 5):
         if i >= di:
             cnt[i] = (cnt[i] + cnt[i-di]) % div_num
+
+
+for i in range(5, n+1):
+    for di in (1, 2, 5):
+        cnt[i] = (cnt[i] + cnt[i-di]) % div_num
 
 print(cnt[-1])
