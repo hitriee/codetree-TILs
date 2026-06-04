766f238acbbc045c308cdc108e06f474c57c621a
나원경 <97578485+hitriee@users.noreply.github.com>
2026-06-04 20:44:50 +0900

    [Passed] 시공의 돌풍 (496ms, 24MB)

--- samsung-sw/시공의 돌풍/heros-of-storm.py
+++ samsung-sw/시공의 돌풍/heros-of-storm.py
@@ -1,6 +1,4 @@
 def main():
-    # 먼지가 인접한 4방향으로 확산 - 원래 먼지 // 5
-    # 모든 먼지 확산 끝낸 다음 해당 칸 더해짐
     def spread():
         temp = [area[i][:] for i in range(n)]
         for i in range(n):
@@ -18,23 +16,17 @@ def main():
 
     # 시공의 돌풍 청소
     def clean(length, arr):
-        # 위칸에서는 반시계 방향, 아래칸에서는 시계 방향
-        # 바람이 불면 먼지가 바람의 방향대로 한 칸씩 이동
-        # 시공의 돌풍으로 들어간 먼지는 사라짐
         val = 0
         
         for i in range(length):
             ny, nx = arr[i]
             val, area[ny][nx] = area[ny][nx], val
-        
-        return val
     
-    # 시공의 돌풍 위치와 이동 순서 찾기
     def find_order():
-        ccw, cw = [], []
-        for k in range(2, n-2):
+        for k in range(2, n-1):
             if area[k][0] == -1:
                 y1, y2 = k, k+1
+                ccw, cw = [], []
                 for j in range(1, m):
                     ccw.append((y1, j))
                     cw.append((y2, j))
@@ -62,14 +54,19 @@ def main():
     n, m, t = map(int, input().split())
     area = [list(map(int, input().split())) for _ in range(n)]
     dy, dx = (-1, 0, 1, 0), (0, -1, 0, 1)
-    total = sum([sum(area[i]) for i in range(n)]) + 2
     ccw, cw = find_order()
     len_ccw, len_cw = len(ccw), len(cw)
     
     for _ in range(t):
         area = spread()
-        total -= (clean(len_ccw, ccw) + clean(len_cw, cw))
+        clean(len_ccw, ccw)
+        clean(len_cw, cw)
+    
+    total = 0
+    for i in range(n):
+        for j in range(m):
+            total += area[i][j]
 
-    return total
+    return total + 2
 
 print(main())
