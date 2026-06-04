c3b1b37b5f00e3456b0e5e576068057bee159e6c
나원경 <97578485+hitriee@users.noreply.github.com>
2026-06-04 11:26:43 +0900

    [Passed] 빙하 (123ms, 20MB)

---
+++ trail4/빙하/glacier.py
@@ -0,0 +1,43 @@
+from collections import deque
+
+n, m = map(int, input().split())
+a = [list(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+dy, dx = (-1, 0, 1, 0), (0, -1, 0, 1)
+water = deque()
+water.append((0, 0))
+visited = [[False] * m for _ in range(n)]
+visited[0][0] = True
+ice = deque()
+duration = last_size = 0
+
+def melt():
+    while water:
+        y, x = water.popleft()
+        for i in range(4):
+            ny, nx = y+dy[i], x+dx[i]
+            if 0 <= ny < n and 0 <= nx < m:
+                if not visited[ny][nx]:
+                    visited[ny][nx] = True
+                    if a[ny][nx] == 1:
+                        ice.append((ny, nx))
+                    else:
+                        water.append((ny, nx))
+    water.extend(ice)
+    ice.clear()
+
+
+
+while True:
+    melt()
+    length = len(water)
+    if length:
+        last_size = length
+    else:
+        break
+    duration += 1
+    
+
+
+print(duration, last_size)
