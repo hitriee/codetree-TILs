2ed71c28a118edd13d1a109322ae45c55b5a4c35
나원경 <97578485+hitriee@users.noreply.github.com>
2026-06-08 15:36:29 +0900

    [Passed] K개의 벽 없애기 (56ms, 17MB)

---
+++ trail4/K개의 벽 없애기/remove-k-walls.py
@@ -0,0 +1,39 @@
+from collections import deque
+
+def int_input(func=int):
+    return map(func, input().split())
+
+def minus_one(num):
+    return int(num) - 1
+
+N, K = int_input()
+arr = [list(int_input()) for _ in range(N)]
+
+r1, c1 = int_input(minus_one)
+r2, c2 = int_input(minus_one)
+
+visited = [[[False] * (K+1) for _ in range(N)] for _ in range(N)]
+
+q = deque()
+dr, dc = (-1, 0, 1, 0), (0, -1, 0, 1)
+q.append((r1, c1, 0, 0))
+visited[r1][c1][0] = True
+
+while q:
+    r, c, time, chance = q.popleft()
+    if r == r2 and c == c2:
+        print(time)
+        break
+    
+    new_time = time + 1
+    for i in range(4):
+        nr, nc = r+dr[i], c+dc[i]
+        if 0 <= nr < N and 0 <= nc < N:
+            new_chance = chance + arr[nr][nc]
+            if new_chance <= K and not visited[nr][nc][new_chance]:
+                visited[nr][nc][new_chance] = True
+                q.append((nr, nc, new_time, new_chance))
+
+else:
+    print(-1)
+
