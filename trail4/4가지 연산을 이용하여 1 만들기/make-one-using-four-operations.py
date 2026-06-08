19b9166428fcc518b5491a8389ca4ce8cd5e95e7
나원경 <97578485+hitriee@users.noreply.github.com>
2026-06-08 15:49:38 +0900

    [Passed] 4가지 연산을 이용하여 1 만들기 (262ms, 66MB)

---
+++ trail4/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
@@ -0,0 +1,28 @@
+N = int(input())
+
+
+# Please write your code here.
+def bfs():
+    from collections import deque
+
+    limit = 3 * N + 1
+    min_cnt = [limit] * limit
+
+    q = deque()
+    min_cnt[1] = 0
+    q.append((1, 0))
+
+    while q:
+        num, cnt = q.popleft()
+        if num == N:
+            return cnt
+
+        new_cnt = cnt + 1
+
+        for new_num in (num + 1, num - 1, num * 2, num * 3):
+            if 0 <= new_num < limit and min_cnt[new_num] > new_cnt:
+                min_cnt[new_num] = new_cnt
+                q.append((new_num, new_cnt))
+
+
+print(bfs())
