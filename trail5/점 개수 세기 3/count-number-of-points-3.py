7305196faaf799103a7218fd751197ba070092b7
나원경 <97578485+hitriee@users.noreply.github.com>
2026-06-09 13:09:45 +0900

    [Passed] 점 개수 세기 3 (127ms, 24MB)

--- trail5/점 개수 세기 3/count-number-of-points-3.py
+++ trail5/점 개수 세기 3/count-number-of-points-3.py
@@ -1,7 +1,12 @@
-n, q = map(int, input().split())
-points = list(map(int, input().split()))
+from sys import stdin
+
+def int_input():
+    return map(int, stdin.readline().split())
+
+n, q = int_input()
+points = list(int_input())
 point_to_idx = {points[i]: i for i in range(n)}
 
 for _ in range(q):
-    a, b = map(int, input().split())
+    a, b = int_input()
     print(point_to_idx[b] - point_to_idx[a] + 1)
