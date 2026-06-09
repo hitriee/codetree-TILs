184476fbd37a3b61114476a1b28e1a39d096424b
나원경 <97578485+hitriee@users.noreply.github.com>
2026-06-09 13:07:34 +0900

    [Passed] 점 개수 세기 3 (408ms, 23MB)

--- trail5/점 개수 세기 3/count-number-of-points-3.py
+++ trail5/점 개수 세기 3/count-number-of-points-3.py
@@ -1,23 +1,8 @@
 n, q = map(int, input().split())
 points = list(map(int, input().split()))
-
-# Please write your code here.
-
-def binary_search(start, end, target):
-    while start <= end:
-        mid = (start + end) // 2
-        mid_num = points[mid]
-        if mid_num == target:
-            return mid
-        elif mid_num < target:
-            start = mid + 1
-        else:
-            end = mid - 1
-
 points.sort()
+point_to_idx = {points[i]: i for i in range(n)}
+
 for _ in range(q):
     a, b = map(int, input().split())
-    left = binary_search(0, n-1, a)
-    right = binary_search(left, n-1, b)
-
-    print(right - left + 1)
+    print(point_to_idx[b] - point_to_idx[a] + 1)
