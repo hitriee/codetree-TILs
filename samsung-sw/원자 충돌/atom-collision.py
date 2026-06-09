a795714579be94ffb86772c33bc4bbc83829f8c9
나원경 <97578485+hitriee@users.noreply.github.com>
2026-06-09 11:14:52 +0900

    [Passed] 원자 충돌 (355ms, 29MB)

--- samsung-sw/원자 충돌/atom-collision.py
+++ samsung-sw/원자 충돌/atom-collision.py
@@ -1,50 +1,47 @@
 # 이동
 def move():
     after_move = []
-    for y, x in atoms:
-        for m, s, d in arr[y][x]:
-            ny, nx = (y + s * dy[d]) % N, (x + s * dx[d]) % N
-            after_move.append((ny, nx, m, s, d))
-    
-    atoms.clear()
-    for i in range(N):
-        for j in range(N):
-            arr[i][j].clear()
+    for y in range(N):
+        for x in range(N):
+            for m, s, d in arr[y][x]:
+                ny, nx = (y + s * dy[d]) % N, (x + s * dx[d]) % N
+                after_move.append((ny, nx, m, s, d))
+            arr[y][x] = []
 
     for ny, nx, m, s, d in after_move:
         arr[ny][nx].append((m, s, d))
-        atoms.add((ny, nx))
 
 # 합성
 def combine():
-    new_total = total
+    for y in range(N):
+        for x in range(N):
+            info = arr[y][x]
+            length = len(info)
+            if length >= 2:
+                nm, ns = info[0][0], info[0][1]
+                remain, is_straight = info[0][2] % 2, True
+                for i in range(1, length):
+                    m, s, d = info[i]
+                    nm += m
+                    ns += s
+                    if is_straight and d % 2 != remain:
+                        is_straight = False
+                
+                if nm >= 5:
+                    nm //= 5
+                    ns //= length
+                    start = 0 if is_straight else 1
+                    arr[y][x] = [(nm, ns, nd) for nd in range(start, 8, 2)]
+                else:
+                    arr[y][x] = []
 
-    before = set(atoms)
-    for y, x in before:
-        info = arr[y][x]
-        length = len(info)
-        if length >= 2:
-            nm, ns = info[0][0], info[0][1]
-            remain, is_straight = info[0][2] % 2, True
-            for i in range(1, length):
-                m, s, d = info[i]
-                nm += m
-                ns += s
-                if is_straight and d % 2 != remain:
-                    is_straight = False
-            
-            new_total -= nm
-            if nm >= 5:
-                nm //= 5
-                new_total += 4*nm
-                ns //= length
-                start = 0 if is_straight else 1
-                arr[y][x] = [(nm, ns, nd) for nd in range(start, 8, 2)]
-            else:
-                atoms.remove((y, x))
-                arr[y][x] = []
-    
-    return new_total
+def calc_total():
+    total = 0
+    for y in range(N):
+        for x in range(N):
+            for m, _, _ in arr[y][x]:
+                total += m
+    return total
 
 
 
@@ -54,19 +51,15 @@ def int_input():
 N, M, K = int_input()
 dy, dx = (-1, -1, 0, 1, 1, 1, 0, -1), (0, 1, 1, 1, 0, -1, -1, -1)
 arr = [[[] for _ in range(N)] for _ in range(N)]
-atoms = set()
-total = 0
 
 for _ in range(M):
     y, x, m, s, d = int_input()
     y -= 1
     x -= 1
     arr[y][x].append((m, s, d))
-    atoms.add((y, x))
-    total += m
 
 for _ in range(K):
     move()
-    total = combine()
+    combine()
 
-print(total)
+print(calc_total())
