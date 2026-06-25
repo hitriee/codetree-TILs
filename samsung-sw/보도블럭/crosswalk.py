0621df95a0d005aef753597af14dde0d6e07b3ef
나원경 <97578485+hitriee@users.noreply.github.com>
2026-06-25 20:06:48 +0900

    [Passed] 보도블럭 (57ms, 17MB)

--- samsung-sw/보도블럭/crosswalk.py
+++ samsung-sw/보도블럭/crosswalk.py
@@ -3,7 +3,6 @@ def int_input():
 
 def can_pass(option):
     cnt = 0
-    # 평지 - 0, 낮아지는 경사로 - 1, 높아지는 경사로 - 2
     for i in range(N):
         before = heights[i][0] if option == 'row' else heights[0][i]
         to_top = to_bottom = False
@@ -12,21 +11,14 @@ def can_pass(option):
             now = heights[i][j] if option == 'row' else heights[j][i]
             if now == before:
                 length += 1
-            # 낮아짐
             elif now == before - 1:
-                # 0 - 1 : 상관 없음
-                # 1 - 1 : 이전 길이가 경사로 길이 이상이어야 함
                 if to_bottom and length < L:
                     break
-                # 2 - 1 : 상관 없음
-                
                 to_top = False
                 to_bottom = True
                 length = 1
             # 높아짐
             elif now == before + 1:
-                # 0 - 2 : 이전 길이가 경사로 길이 이상이어야 함
-                # 1 - 2 : 이전 길이가 2 * 경사로 길이 이상이어야 함
                 if to_bottom:
                     if length < 2*L:
                         break
@@ -34,9 +26,7 @@ def can_pass(option):
                     break
                 to_bottom = False
                 length = 1
-                # 2 - 2 : 상관 없음
                 to_top = True
-            # 높이 차이가 1보다 큰 경우
             else:
                 break
             before = now
