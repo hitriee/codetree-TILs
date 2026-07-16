46351bf65c494107c7aab23b01a11f9f9c2e8230
나원경 <97578485+hitriee@users.noreply.github.com>
2026-07-16 20:56:12 +0900

    [Passed] 이상한 다트 게임 (151ms, 22MB)

---
+++ samsung-sw/이상한 다트 게임/odd-dart-game.py
@@ -0,0 +1,79 @@
+from collections import deque
+
+def int_input():
+    return map(int, input().split())
+
+def remove():
+    removed_total = minus = 0
+    minus_set = set()
+    if total:
+        for j in range(M):
+            number = arr[0][j]
+            if number:
+                nj = (j-1) % M
+                if arr[0][nj] == number:
+                    
+                    minus_set.add((0, nj))
+                    minus_set.add((0, j))
+                
+                    
+        for i in range(1, N):
+            for j in range(M):
+                number = arr[i][j]
+                if number:
+                    has_same_num = False
+                    for idx in range(2):
+                        ni, nj = (i+di[idx]) % N, (j+dj[idx]) % M
+                        if arr[ni][nj] == number:
+                            minus_set.add((ni, nj))
+                            has_same_num = True
+                    if has_same_num:
+                        minus_set.add((i, j))
+                    
+        if minus_set:
+            for i, j in minus_set:
+                removed_total += arr[i][j]
+                arr[i][j] = 0
+        else:
+            avg = total // cnt
+            
+            for i in range(N):
+                for j in range(M):
+                    number = arr[i][j]
+                    if number:
+                        if number > avg:
+                            arr[i][j] -= 1
+                            removed_total += 1
+                        elif number < avg:
+                            arr[i][j] += 1
+                            removed_total -= 1
+        
+    return (len(minus_set), removed_total)
+
+
+def rotate(x, d, k):
+    for i in range(x-1, N, x):
+        temp = arr[i]
+        if d == 0:
+            for _ in range(k):
+                temp.appendleft(temp.pop())
+        else:
+            for _ in range(k):
+                temp.append(temp.popleft())
+    
+    return remove()
+    
+
+
+N, M, Q = int_input()
+arr = [deque(int_input()) for _ in range(N)]
+di, dj = (0, -1,), (-1, 0)
+cnt = N*M
+total = sum([sum(arr[i]) for i in range(N)])
+
+for _ in range(Q):
+    cnt_minus, minus = rotate(*int_input())
+    cnt -= cnt_minus
+    total -= minus
+
+print(total)
