454ee9be3ceb33dd01aacc6b3ac14180d08267b4
나원경 <97578485+hitriee@users.noreply.github.com>
2026-07-22 20:52:54 +0900

    [Passed] 회전하는 빙하 (888ms, 26MB)

---
+++ samsung-sw/회전하는 빙하/rotating-glacier.py
@@ -0,0 +1,93 @@
+def int_input():
+    return map(int, input().split())
+
+def in_range(y, x):
+    return 0 <= y < M and 0 <= x < M
+
+def divide_conquer(now, target, ly, lx):
+    if now == target:
+        half = now // 2
+        temp = [list(area[i][lx:lx + half]) for i in range(ly, ly + half)]
+        for k in range(3, 0, -1):
+            nly, nlx = ly + start_y[k] * half, lx + start_x[k] * half
+            arr = [list(area[i][nlx:nlx + half]) for i in range(nly, nly + half)]
+            rotate(nly, nlx, half, k, arr)
+        rotate(ly, lx, half, 0, temp)
+        return
+
+    half = now // 2
+    for k in range(3, -1, -1):
+        nly, nlx = ly + start_y[k] * half, lx + start_x[k] * half
+        divide_conquer(half, target, nly, nlx)
+
+def rotate(s_i, s_j, length, idx, arr):
+    di, dj = dy[idx] * length, dx[idx] * length
+    for i in range(s_i, s_i + length):
+        ni = i + di
+        for j in range(s_j, s_j + length):
+            nj = j + dj
+            area[ni][nj] = arr[i - s_i][j - s_j]
+
+
+def melt():
+    path = []
+    for y in range(M):
+        for x in range(M):
+            if area[y][x]:
+                cnt = 0
+                for k in range(4):
+                    ny, nx = y+dy[k], x+dx[k]
+                    if not in_range(ny, nx) or area[ny][nx] == 0:
+                        cnt += 1
+                        if cnt >= 2:
+                            path.append((y, x))
+                            break
+
+    for y, x in path:
+        area[y][x] -= 1
+
+
+def bfs():
+    from collections import deque
+    q = deque()
+    visited = [[False] * M  for _ in range(M)]
+    max_cnt = total = 0
+    for i in range(M):
+        for j in range(M):
+            if not visited[i][j] and area[i][j]:
+                cnt = 0
+                visited[i][j] = True
+                q.append((i, j))
+                while q:
+                    y, x = q.popleft()
+                    total += area[y][x]
+                    cnt += 1
+
+                    for k in range(4):
+                        ny, nx = y+dy[k], x+dx[k]
+                        if in_range(ny, nx) and not visited[ny][nx] and area[ny][nx]:
+                            visited[ny][nx] = True
+                            q.append((ny, nx))
+
+                if cnt > max_cnt:
+                    max_cnt = cnt
+
+    return f'{total}\n{max_cnt}'
+
+
+N, _ = int_input()
+levels = [1]
+for _ in range(N):
+    levels.append(levels[-1] * 2)
+M = levels[-1]
+area = [list(int_input()) for _ in range(levels[-1])]
+queries = list(int_input())
+start_y, start_x = (0, 0, 1, 1), (0, 1, 1, 0)
+dy, dx = (0, 1, 0, -1), (1, 0, -1, 0)
+
+for num in queries:
+    if num != 0:
+        divide_conquer(M, levels[num], 0, 0)
+    melt()
+
+print(bfs())
